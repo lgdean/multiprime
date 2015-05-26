a77e740a18dcca24b867a60a533d220648878212
Laura Dean <lgdean@users.noreply.github.com>
2015-05-25 23:00:03 -0700

    hey, now it generates primes

--- src/multiprime/core.clj
+++ src/multiprime/core.clj
@@ -6,7 +6,18 @@
   [& args]
   (println "Hello, World!"))
 
+;; it came from the REPL... and is cool but a little hard to follow.
+(defn one-and-primes
+  []
+  (let [divisible-by? (fn [d] (fn [n] (= 0 (mod n d))))
+        remove-multiples-of (fn [x s] (remove (divisible-by? x) s))
+        range-starting-from (fn [x] (drop x (range)))
+        next-item (fn [foo] (cons (first (rest foo))
+                                  (remove-multiples-of (first (rest foo))
+                                                       (rest (rest foo)))))]
+    (map first (iterate next-item (range-starting-from 1)))))
+
 (defn primes
   "Return the first n primes."
   [n]
-  [2,3,5])
+  (take n (drop 1 (one-and-primes))))
