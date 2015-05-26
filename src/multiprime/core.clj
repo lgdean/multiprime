88e2f9974fda0837e295cba268069a15fd28d767
Laura Dean <lgdean@users.noreply.github.com>
2015-05-26 10:50:35 -0700

    looks like lazy-seq is the right thing. and pretty cool!

--- src/multiprime/core.clj
+++ src/multiprime/core.clj
@@ -6,17 +6,16 @@
   [& args]
   (println "Hello, World!"))
 
-;; it came from the REPL... and is cool but a little hard to follow.
-(defn one-and-primes
+(defn all-primes
   []
   (let [divisible-by? (fn [d] (fn [n] (= 0 (mod n d))))
         remove-multiples-of (fn [x s] (remove (divisible-by? x) s))
         range-starting-from (fn [x] (drop x (range)))
-        next-item (fn [[_ & [y & ys]]]
-                    (cons y (remove-multiples-of y ys)))]
-    (map first (iterate next-item (range-starting-from 1)))))
+        rel-prime (fn rel-prime [[x & xs]]
+                    (cons x (lazy-seq (rel-prime (remove-multiples-of x xs)))))]
+    (rel-prime (range-starting-from 2))))
 
 (defn primes
   "Return the first n primes."
   [n]
-  (take n (drop 1 (one-and-primes))))
+  (take n (all-primes)))
