bb304c66032cc0bfe8da8120b3fc30abd8e0e801
Laura Dean <lgdean@users.noreply.github.com>
2015-05-27 16:36:19 -0700

    slightly better test

--- test/multiprime/core_test.clj
+++ test/multiprime/core_test.clj
@@ -4,4 +4,4 @@
 
 (deftest a-test
   (testing "primes 10 should return first 10 primes."
-    (is (= (primes 10) [2,3,5,7,11,13,17,19,23,29]))))
+    (is (= (primes 10) '(2 3 5 7 11 13 17 19 23 29)))))
