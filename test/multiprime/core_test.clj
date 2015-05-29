003b9f878a32ffb752807468af140846b5c8687b
Laura Dean <lgdean@users.noreply.github.com>
2015-05-28 23:17:10 -0400

    added integration test, implying a command-line arg I may not actually want

--- test/multiprime/core_test.clj
+++ test/multiprime/core_test.clj
@@ -5,3 +5,7 @@
 (deftest a-test
   (testing "primes 10 should return first 10 primes."
     (is (= (primes 10) '(2 3 5 7 11 13 17 19 23 29)))))
+
+(deftest integration-test
+  (testing "output should be a multiplication table."
+    (is (= "X\t2\n2\t4\n" (with-out-str (-main 1))))))
