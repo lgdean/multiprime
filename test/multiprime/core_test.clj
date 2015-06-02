0190c6a94a00bd5bf4eca14b509a168b1673a1b6
Laura Dean <lgdean@users.noreply.github.com>
2015-06-02 18:09:19 -0400

    added a test, and comments about the small number of tests.

--- test/multiprime/core_test.clj
+++ test/multiprime/core_test.clj
@@ -21,6 +21,17 @@
              (times-table [1 2 5]))))))
 
 (deftest integration-test
-  (testing "output should be a multiplication table."
-    (is (= ["X\t2\t3" "2\t4\t6" "3\t6\t9"]
-           (clojure.string/split-lines (with-out-str (-main "2")))))))
+  (testing "output"
+    (testing "should be a multiplication table."
+      (is (= ["X\t2\t3" "2\t4\t6" "3\t6\t9"]
+             (clojure.string/split-lines (with-out-str (-main "2"))))))
+    (testing "should print 11 rows (for 10 primes) by default"
+      (is (= 11 (count (clojure.string/split-lines (with-out-str (-main)))))))))
+
+;; Yup, that's it.  I feel like I'd normally write more tests.
+;; And yet, these tests are enough to let me feel confident in the code.
+;; That speaks pretty well for Clojure.
+
+;; I also did some playing in the REPL, but I think everything needed
+;; from there made it here.  A possible exception is performance
+;; testing / checking for lack of stack overflow.
